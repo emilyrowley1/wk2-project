da6e7df859b8990b2cebcca28b2af4fbced8af91
Emily Rowley <erowley49@gmail.com>
2023-05-01 13:08:45 -0600

    update db name

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -13,7 +13,7 @@ const getSingle = async (req, res, next) => {
   const userId = new ObjectId(req.params.id);
   const result = await mongodb
     .getDb()
-    .db('Web_Services')
+    .db('contacts')
     .collection('contacts')
     .find({ _id: userId });
   result.toArray().then((lists) => {
