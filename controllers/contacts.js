a38902f64a665662316232aead89799c457e7ac8
Emily Rowley <erowley49@gmail.com>
2023-05-09 11:14:30 -0600

    CRUD operations

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -22,4 +22,65 @@ const getSingle = async (req, res, next) => {
   });
 };
 
-module.exports = { getAll, getSingle };
+const createContact = async (req, res, next) => {
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+
+  const result = await mongodb
+    .getDb()
+    .db('contacts')
+    .collection('contacts')
+    .insertOne(contact);
+
+    if (result.acknowledged) {
+      res.status(201).json(result);
+    } else {
+      res.status(500).json(result.error || 'Some error occurred while creating the contact.');
+    }
+};
+
+const updateContact = async (req, res, next) => {
+  console.log("I got here");
+  const userId = new ObjectId(req.params.id);
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+
+  const response = await mongodb
+    .getDb()
+    .db('contacts')
+    .collection('contacts')
+    .replaceOne({ _id: userId }, contact);
+  console.log(response);
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while updating the contact.');
+  }
+};
+
+const deleteContact = async (req, res, next) => {
+  const userId = new ObjectId(req.params.id);
+  const result = await mongodb
+    .getDb()
+    .db('contacts')
+    .collection('contacts')
+    .deleteOne({ _id: userId });
+
+    if (result.deletedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(500).json(result.error || 'Some error occurred while updating the contact.');
+    }
+};
+
+module.exports = { getAll, getSingle, createContact, updateContact, deleteContact };
